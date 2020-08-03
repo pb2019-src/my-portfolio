fcb6e84a2fa45b88ad3af2f4bd6e1aba865e4915
Ahmad Almanassra <Ahmadalmanassra@gmail.com>
2020-08-04 03:24:20 +0800

    Use SNS to notify via email when portfoliois deployed successfully or otherwise

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,21 +6,27 @@ import mimetypes
 
 def lambda_handler(event, context):
 
+    sns=boto3.resource('sns')
+    topic=sns.Topic('arn:aws:sns:us-east-1:476314331207:deployPortfolioTopic')
 
-    s3 = boto3.resource('s3')
-    portfolio_bucket =s3.Bucket('portfoilio.ahmadalmanassra.info')
-    build_bucket=s3.Bucket('portfoliobuild.ahmadalmanassra.info')
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket =s3.Bucket('portfoilio.ahmadalmanassra.info')
+        build_bucket=s3.Bucket('portfoliobuild.ahmadalmanassra.info')
 
-    #try StringIO first, if not try BytesIO
-    portfolio_zip=io.BytesIO()
-    build_bucket.download_fileobj('myPortfolio.zip',portfolio_zip)
+        #try StringIO first, if not try BytesIO
+        portfolio_zip=io.BytesIO()
+        build_bucket.download_fileobj('myPortfolio.zip',portfolio_zip)
 
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj =myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj =myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print("Job done!")
+        topic.publish(Subject="Portfolio Deployed",Message="Portfolio deployed successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deployed Failed", Message="The portfolio was not deployed successfully!")
     return 'Hello from Lambda'
