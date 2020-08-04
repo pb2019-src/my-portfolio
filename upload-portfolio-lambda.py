725a04393bca13f95a4bdd3e16545d2f68cec865
Ahmad Almanassra <Ahmadalmanassra@gmail.com>
2020-08-05 01:53:53 +0800

    Use bucket name and object key from codepipline, and notifty when codepipline job is done

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,3 +1,4 @@
+import json
 import boto3
 #from botocore.client import Config
 import io
@@ -8,15 +9,25 @@ def lambda_handler(event, context):
 
     sns=boto3.resource('sns')
     topic=sns.Topic('arn:aws:sns:us-east-1:476314331207:deployPortfolioTopic')
+    location={
+        "bucketName": 'portfoliobuild.ahmadalmanassra.info',
+        "objectKey": 'myPortfolio.zip'
+    }
 
     try:
+        job = event.get("CodePipeline.job")
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"]=="BuildArtifact":
+                    location =artifact["location"]["s3Location"]
+        print ("Building portfolio from"+str(location))
         s3 = boto3.resource('s3')
         portfolio_bucket =s3.Bucket('portfoilio.ahmadalmanassra.info')
-        build_bucket=s3.Bucket('portfoliobuild.ahmadalmanassra.info')
+        build_bucket=s3.Bucket(location["bucketName"])
 
         #try StringIO first, if not try BytesIO
         portfolio_zip=io.BytesIO()
-        build_bucket.download_fileobj('myPortfolio.zip',portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"],portfolio_zip)
 
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
@@ -27,6 +38,10 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         print("Job done!")
         topic.publish(Subject="Portfolio Deployed",Message="Portfolio deployed successfully!")
+        if job:
+            codepipeline=boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Portfolio Deployed Failed", Message="The portfolio was not deployed successfully!")
+        raise
     return 'Hello from Lambda'
